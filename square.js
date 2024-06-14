41a63aa5b976dedc8634eaa2277f90e6ee8df952
Archana-2020wa86953 <167838712+Archana-2020wa86953@users.noreply.github.com>
2024-06-14 12:42:07 +0530

    Add files via upload

---
+++ square.js
@@ -0,0 +1,8 @@
+// This is a simple JavaScript program to calculate the square of a number
+ 
+function square(number) {
+    return number * number;
+}
+ 
+// Example usage:
+console.log(square(5)); // Output: 25
